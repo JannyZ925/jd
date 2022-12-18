d890d0161b7aa43abb7c8efc3519b9472148399a
Janny <janny_z@foxmail.com>
2022-12-18 14:00:28 +0800

    feat：完成了首页的开发

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,33 +1,113 @@
 <template>
-	<view>
-		<text class="title">{{title}}</text>
-	</view>
+  <view>
+    <!-- 轮播图区域 -->
+    <swiper
+      :indicator-dots="true"
+      :autoplay="true"
+      :circular="true"
+      :interval="3000"
+    >
+      <swiper-item v-for="(item, index) in bannerList" :key="item.goodsId">
+        <image :src="item.imageUrl" />
+      </swiper-item>
+    </swiper>
+
+    <!-- 分类导航区域 -->
+    <view class="menu-list">
+      <view class="menu-item" v-for="(item, index) in menuList" :key="item.id">
+        <image :src="item.imageUrl"></image>
+      </view>
+    </view>
+
+    <!-- 瀑布流区域 -->
+    <view class="floor-list">
+      <view class="floor-item" v-for="(floor, index) in floorList" :key="index">
+        <!-- 楼层名 -->
+        <image class="floor-title" :src="floor.floorTitle.imageUrl"></image>
+        <!-- 商品列表 -->
+        <view class="floor-img-box">
+          <!-- 左边大盒子 -->
+          <view class="left-img-box">
+            <image
+              :src="floor.productList[0].imageUrl"
+              :style="{ width: floor.productList[0].imageWidth/2 + 'px' }"
+              mode="widthFix"
+            >
+            </image>
+          </view>
+          <!-- 右边四个小盒子 -->
+          <view class="right-img-box">
+            <view
+              v-for="(product, index) in floor.productList"
+              :key="index"
+              v-show="index !== 0"
+            >
+              <image
+                :src="product.imageUrl"
+                :style="{ width: product.imageWidth/2 + 'px' }"
+                mode="widthFix"
+              >
+              </image>
+            </view>
+          </view>
+        </view>
+      </view>
+    </view>
+  </view>
 </template>
 
 <script>
-import Taro from '@tarojs/taro'
-// import { getBannerList } from '@/api/home.js'
+import Taro from "@tarojs/taro";
+import { request, loading } from "../../utils/request";
+import "./index.less";
+
 export default {
-	data() {
-		return {
-			title: '首页',
-		}
-	},
-	methods: {
-		// getBannerList() {
-		// 	const result = Taro.request({
-		// 		url: 'http://localhost:5200/api/jd-service/home/bannerList',
-		// 		method: 'get'
-		// 	})
-		// 	console.log(result)
-		// }
-	},
-	mounted() {
-		// this.getBannerList()
-	}
-}
+  data() {
+    return {
+      // 轮播图
+      bannerList: [],
+      // 分类导航
+      menuList: [],
+      // 瀑布流
+      floorList: [],
+    };
+  },
+  methods: {
+    // 获取轮播图数据
+    getBannerList() {
+      // 发起请求，获取轮播图数据
+      return request("/home/bannerList");
+    },
+
+    // 获取分类导航数据
+    getMenuList() {
+      // 发起请求，获取分类导航数据
+      return request("/home/menuList");
+    },
+
+    // 获取瀑布流数据
+    getFloorList() {
+      // 发起请求，获取瀑布流数据
+      return request("/home/floorList");
+    },
+  },
+
+  mounted() {
+    // 提示加载中
+    loading(true);
+
+    // 发请求，给对应数据赋值
+    Promise.all([
+      this.getBannerList(),
+      this.getMenuList(),
+      this.getFloorList(),
+    ]).then(([bannerList, menuList, floorList]) => {
+      this.bannerList = bannerList;
+      this.menuList = menuList;
+      this.floorList = floorList;
+      // 请求完毕后关闭加载中提示
+      loading(false);
+    });
+  },
+};
 </script>
-
-<style lang="less">
-
-</style>
