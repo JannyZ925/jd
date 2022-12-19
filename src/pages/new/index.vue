6ab67e6cc96816577b941f7b8c634be906d03788
Janny <janny_z@foxmail.com>
2022-12-19 23:43:55 +0800

    feat：完成了新品页的开发

--- src/pages/new/index.vue
+++ src/pages/new/index.vue
@@ -1,17 +1,24 @@
 <template>
-  <view class="new">
-    <text>{{ msg }}</text>
+  <view>
+    <view style="min-height: 100vh">
+      <goods-item v-for="(goods, index) in goodsList" :key="goods.goodsId" :goods="goods"></goods-item>
+    </view>
+    <view class="tips" v-if="showTips">我是有底线的~</view>  
   </view>
+  
 </template>
 
 <script>
-import './index.less'
+import "./index.less";
+import mixin from "../../mixin/index";
 
 export default {
-  data () {
-    return {
-      msg: '新品'
-    }
-  }
-}
+  async onLoad(options) {
+    // 获取页面的参数
+    this.queryObj.isNew = true;
+    // 发起请求
+    await this.getGoodsList();
+  },
+  mixins: [mixin]
+};
 </script>
