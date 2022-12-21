abc75c0d75137719a424f54e2cf9d9cd19231298
Janny <janny_z@foxmail.com>
2022-12-21 15:10:45 +0800

    feat：封装search组件

---
+++ src/components/search/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <view class="search-container">
+    <view class="search-input">
+      <icon class="search-icon" type="search" size="20"/>
+      <text class="search-placeholder">搜索</text>
+    </view>
+  </view>
+</template>
+
+<script>
+import "./index.less";
+
+export default {
+  data() {
+    return {
+      
+    };
+  }
+};
+</script>
