c7ae98bfb07ef1a878efcb56eb709a17e17d5fcd
Janny <janny_z@foxmail.com>
2022-12-28 23:44:55 +0800

    feat：“我的”页面

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import TaroUIVue from '@lexmin0412/taro-ui-vue/src/index'
-import { AtSearchBar, AtTag } from "@lexmin0412/taro-ui-vue";
+import { AtSearchBar, AtTag, AtForm, AtInput } from "@lexmin0412/taro-ui-vue";
 import './request'
 
 import GoodsItem from './components/goodsItem/index.vue';
@@ -14,6 +14,8 @@ Vue.use(TaroUIVue)
 // 注册全局组件
 Vue.component(AtSearchBar)
 Vue.component(AtTag)
+Vue.component(AtForm)
+Vue.component(AtInput)
 Vue.component('goods-item', GoodsItem)
 Vue.component('search', Search)
 
--- src/pages/my/index.vue
+++ src/pages/my/index.vue
@@ -1,8 +1,28 @@
 <template>
   <view class="my">
-    <text>{{ msg }}</text>
-    <AtButton type='primary'>按钮文案</AtButton>
-    <AtSearchBar />
+    <AtForm class="form">
+      <AtInput 
+        name='phone' 
+        title='手机号' 
+        type='phone' 
+        placeholder='请输入手机号' 
+        :value="user.phone"
+        :onChange="onChange.bind(this, 'phone')" 
+        clear
+      />
+      <AtInput 
+        name='password' 
+        title='密码' 
+        type='password' 
+        placeholder='请输入密码' 
+        :value="user.password"
+        :onChange="onChange.bind(this, 'password')" 
+        clear
+      />
+      <AtButton type="primary" class="submitBtn" :onClick="handleClickSubmitBtn">登录</AtButton>
+    </AtForm>
+    
+    <view class="tip">未注册的手机号登录成功后将自动注册</view>
   </view>
 </template>
 
@@ -10,15 +30,26 @@
 import './index.less'
 
 import { AtButton, AtSearchBar } from '@lexmin0412/taro-ui-vue'
+import { request, loading } from "../../utils/request";
 
 export default {
-  components: {
-    AtButton,
-    AtSearchBar
-  },
   data () {
     return {
-      msg: '我的'
+      // 用户的登录/注册信息
+      user: {
+        phone: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    // 输入框的值改变
+    onChange(stateName, value) {
+      this.user[stateName] = value;
+    },
+
+    async handleClickSubmitBtn() {
+      const res = await request('/user/add', "POST", this.user)
     }
   }
 }
