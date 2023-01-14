af8c4891ec4879b243a72445100df5473acd9ae3
Janny <janny_z@foxmail.com>
2023-01-14 21:44:44 +0800

    feat: 修改goodsItem组件

--- src/components/goodsItem/index.vue
+++ src/components/goodsItem/index.vue
@@ -1,11 +1,17 @@
 <template>
-  <view class="goods-item">
-    <view class="goods-img" @tap="handleClickImage(goods.goodsId)">
-      <image :src="goods.goodsSmallLogo || defaultImage" />
-    </view>
-    <view class="goods-info">
-      <text class="goods-name">{{ goods.goodsName }}</text>
-      <text class="goods-price">￥{{ goods.goodsPrice }}</text>
+  <view class="goods-item-container">
+    <radio v-if="isCart" color="#6495ED" :checked="goods.goodsState" @tap="changeGoodsState"/>
+    <view class="goods-item">
+      <view class="goods-img" @tap="handleClickImage(goods.goodsId)">
+        <image class="goods-img" :src="goods.goodsSmallLogo || defaultImage"></image>
+      </view>
+      <view class="goods-info">
+        <text class="goods-name">{{ goods.goodsName }}</text>
+        <view>
+          <text class="goods-price">￥{{ goods.goodsPrice }}</text>
+          <AtInputNumber v-if="isCart" :min="1" :value="goods.goodsCount" :onChange="changeGoodsCount.bind(this)"/>
+        </view>
+      </view>
     </view>
   </view>
 </template>
@@ -14,7 +20,7 @@
 import "./index.less";
 import Taro from '@tarojs/taro'
 import * as paths from "@/consts/path";
-import { addLookedGoods } from "@/apis/user";
+import { addLookedGoods, updateGoodsCountInCart, updateGoodsStateInCart } from "@/apis/user";
 import { mapMutations, mapState } from 'vuex';
 
 export default {
@@ -33,6 +39,11 @@ export default {
     goods: {
       type: Object,
       default: {}
+    },
+    // 是否为购物车页面,用于判断是否展示radio和数字输入框
+    isCart: {
+      type: Boolean,
+      default: false
     }
   },
   methods: {
@@ -46,7 +57,19 @@ export default {
         const user = await addLookedGoods(this.user, this.goods.goodsId);
         this.updateUser(user)
       }
-    }
+    },
+
+    async changeGoodsCount(value) {
+      this.goods.goodsCount = value;
+      const user = await updateGoodsCountInCart(this.user, this.goods)
+      this.updateUser(user)
+    },
+
+    async changeGoodsState() {
+      this.goods.goodsState = !this.goods.goodsState;
+      const user = await updateGoodsStateInCart(this.user, this.goods)
+      this.updateUser(user)
+    },
   }
 };
 </script>
